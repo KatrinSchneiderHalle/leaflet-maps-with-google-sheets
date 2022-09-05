3f07b4a8ab8b0c6e4bf0f69a172beb360e10f3fd
KatrinSchneiderHalle <112479722+KatrinSchneiderHalle@users.noreply.github.com>
2022-09-05 17:31:13 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/13HxO6zP7q_lQ2STFU7RIONHz_Q7uonz_oiyZudc_QcA/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1yLMIDoIpLldwxvDEgUsfNIOdhUDuDF9ygRrzmmTTrHs/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
